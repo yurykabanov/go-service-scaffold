798aa39da27b8dd95f575160e553fb3014d1c432
Yury Kabanov <yury.kabanov@gmail.com>
2020-02-15 16:41:42 +0500

    add todos for build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,12 @@ BUILD_FLAG="$VERSION_PKG.Build=${DRONE_COMMIT=unknown}"
 VERSION_FLAG="$VERSION_PKG.Version=${DRONE_TAG=no_tag}"
 BUILD_DATE_FLAG="$VERSION_PKG.BuildDate=$BUILD_DATE"
 
+# TODO: dependencies?
+#  go download ?
+
+# TODO: fix paths?
+# this works, but take so long to complete:
+#   swag init -g ./cmd/server/main.go -o ../../docs -d ../../ --parseDependency
 go generate ./cmd/server/main.go
 
 go build \
